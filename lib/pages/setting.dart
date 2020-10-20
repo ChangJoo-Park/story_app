3ec80dc6fb6d76fd46eba321745fecec9405adfc
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2020-10-20 20:05:18 +0900

    settings ui 수정

---
+++ lib/pages/setting.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+class SettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade100,
+        title: Text('설정'),
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: 'Common',
+            tiles: [
+              SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: Icon(Icons.language),
+              ),
+              SettingsTile(
+                title: 'Environment',
+                subtitle: 'Production',
+                leading: Icon(Icons.cloud_queue),
+                onTap: () => print('e'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: 'Account',
+            tiles: [
+              SettingsTile(title: 'Phone number', leading: Icon(Icons.phone)),
+              SettingsTile(title: 'Email', leading: Icon(Icons.email)),
+              SettingsTile(title: 'Sign out', leading: Icon(Icons.exit_to_app)),
+            ],
+          ),
+          SettingsSection(
+            title: 'Security',
+            tiles: [
+              SettingsTile.switchTile(
+                title: 'Lock app in background',
+                leading: Icon(Icons.phonelink_lock),
+                switchValue: false,
+                onToggle: (bool value) {},
+              ),
+              SettingsTile.switchTile(
+                  title: 'Use fingerprint',
+                  leading: Icon(Icons.fingerprint),
+                  onToggle: (bool value) {},
+                  switchValue: false),
+              SettingsTile.switchTile(
+                title: 'Change password',
+                leading: Icon(Icons.lock),
+                switchValue: true,
+                onToggle: (bool value) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Enable Notifications',
+                enabled: false,
+                leading: Icon(Icons.notifications_active),
+                switchValue: true,
+                onToggle: (value) {},
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: 'Misc',
+            tiles: [
+              SettingsTile(
+                  title: 'Terms of Service', leading: Icon(Icons.description)),
+              SettingsTile(
+                  title: 'Open source licenses',
+                  leading: Icon(Icons.collections_bookmark)),
+            ],
+          ),
+          CustomSection(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 22, bottom: 8),
+                  child: Image.asset(
+                    'assets/settings.png',
+                    height: 50,
+                    width: 50,
+                    color: Color(0xFF777777),
+                  ),
+                ),
+                Text(
+                  'Version: 2.4.0 (287)',
+                  style: TextStyle(color: Color(0xFF777777)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
