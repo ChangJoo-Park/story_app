ac3dc4885f31c6594556843b59f5fa0da16d295e
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2020-10-20 15:47:58 +0900

    WIP compose

---
+++ lib/pages/compose.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ComposePage extends StatefulWidget {
+  @override
+  _ComposePageState createState() => _ComposePageState();
+}
+
+class _ComposePageState extends State<ComposePage> {
+  final _formKey = GlobalKey<FormState>();
+  String title = '';
+  String description = '';
+  DateTime date;
+  TimeOfDay timeOfDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('새 글쓰기'),
+      ),
+      persistentFooterButtons: [
+        IconButton(
+          icon: Icon(Icons.calendar_today),
+          onPressed: () {
+            showDatePicker(
+              context: context,
+              firstDate: DateTime.now().subtract(Duration(days: 365 * 10)),
+              lastDate: DateTime.now().add(Duration(days: 365 * 10)),
+              initialDate: DateTime.now(),
+            ).then((newDate) {
+              if (newDate == null) {
+                return;
+              }
+              setState(() {
+                date = newDate;
+              });
+            });
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.access_time),
+          onPressed: () {
+            showTimePicker(
+              context: context,
+              initialTime: TimeOfDay.now(),
+            ).then((newTime) {
+              if (newTime == null) {
+                return;
+              }
+              setState(() {
+                timeOfDay = newTime;
+              });
+            }).catchError((e) {
+              print(e);
+            });
+          },
+        ),
+        // IconButton(
+        //   icon: Icon(Icons.photo),
+        //   onPressed: () {
+        //     Get.back();
+        //   },
+        // ),
+        IconButton(
+          icon: Icon(Icons.save),
+          onPressed: () async {
+            // Keyboard Hide
+            FocusScope.of(context).unfocus();
+            // 여기서 조립
+            _formKey.currentState.save();
+            final directory = await getApplicationDocumentsDirectory();
+            final filename = DateTime.now().toUtc();
+            final path = '${directory.path}/notes/$filename.txt';
+            File(path).createSync();
+            File(path).writeAsStringSync('''---
+title: "$title"
+excerpt: "$excerpt"
+datetime: "${DateTime.now().toUtc()}"
+images: ""
+---
+$description''');
+            Get.back();
+          },
+        ),
+      ],
+      body: ScrollConfiguration(
+        behavior: NoGlowScrollBehavior(),
+        child: Form(
+          key: _formKey,
+          child: Flex(
+            direction: Axis.vertical,
+            children: [
+              Expanded(
+                flex: 0,
+                child: TextFormField(
+                  cursorColor: Colors.black,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    hintText: '제목을 적어주세요',
+                    contentPadding: const EdgeInsets.only(
+                        left: 16, right: 16, top: 8, bottom: 16),
+                  ),
+                  onSaved: (value) => setState(() => title = value),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: TextFormField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    hintText: "이야기를 적어주세요",
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.only(
+                        left: 16, right: 16, top: 8, bottom: 16),
+                  ),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 99999,
+                  autofocus: false,
+                  onSaved: (value) => setState(() => description = value),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String get excerpt {
+    String plainText = description.split('\n').join();
+    if (plainText.length > 300) {
+      plainText = plainText.substring(0, 300);
+    }
+    return plainText;
+  }
+}
+
+class NoGlowScrollBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
