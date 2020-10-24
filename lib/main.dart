7c15c4213d8eb9fdca026b83482dc1d247e69f57
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2020-10-24 10:02:52 +0900

    settings 추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,23 @@ import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:story_app/controllers/home_controller.dart';
 import 'package:story_app/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+  if (!_prefs.containsKey('initialized') || !_prefs.getBool('initialized')) {
+    await Future.wait([
+      _prefs.setBool('initialized', true),
+      _prefs.setString('font', 'Mapo금빛나루'),
+      _prefs.setString('locale', 'ko'),
+    ]);
+  }
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
@@ -27,15 +38,17 @@ void main() async {
       ],
       theme: ThemeData(
         primaryColor: Colors.grey.shade50,
-        fontFamily: 'Mapo금빛나루',
+        fontFamily: _prefs.getString('font'),
         accentColor: Colors.grey.shade50,
         appBarTheme: AppBarTheme(
           color: Colors.grey.shade50,
           elevation: 0,
           iconTheme: IconThemeData(color: Colors.grey.shade800),
           textTheme: TextTheme(
-            headline6:
-                TextStyle(color: Colors.grey.shade600, fontFamily: 'Mapo금빛나루'),
+            headline6: TextStyle(
+              color: Colors.grey.shade600,
+              fontFamily: _prefs.getString('font'),
+            ),
           ),
         ),
         floatingActionButtonTheme: FloatingActionButtonThemeData(
--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -3,11 +3,22 @@ import 'package:get/get.dart';
 import 'package:in_app_review/in_app_review.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:share/share.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Map<String, String> locale = {
+  'ko': '한국어',
+  'en': 'English',
+};
+
+Map<String, String> font = {
+  'Mapo금빛나루': '마포 금빛나루',
+};
 
 class SettingPage extends StatelessWidget {
   final TextStyle settingTitleStyle = TextStyle(color: Colors.grey.shade800);
   final TextStyle settingSectionTitleStyle =
       TextStyle(color: Colors.grey.shade700);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,92 +26,120 @@ class SettingPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('더보기'),
       ),
-      body: SettingsList(
-        backgroundColor: Colors.grey.shade50,
-        shrinkWrap: true,
-        sections: [
-          SettingsSection(
-            titleTextStyle: settingSectionTitleStyle,
-            title: '일반 설정',
-            tiles: [
-              SettingsTile(
-                title: '언어',
-                subtitle: '한국어',
-                leading: Icon(Icons.language),
-                titleTextStyle: settingTitleStyle,
-              ),
-              SettingsTile(
-                title: '글꼴',
-                subtitle: '마포금빛나루',
-                leading: Icon(Icons.cloud_queue),
-                titleTextStyle: settingTitleStyle,
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: '연락',
-            titleTextStyle: settingSectionTitleStyle,
-            tiles: [
-              SettingsTile(
-                title: '플레이스토어에 리뷰 남기기',
-                subtitle: '칭찬과 격려를 남겨주세요.',
-                leading: Icon(Icons.star_rate),
-                titleTextStyle: settingTitleStyle,
-                onTap: () async {
-                  try {
-                    final InAppReview inAppReview = InAppReview.instance;
-                    if (await inAppReview.isAvailable()) {
-                      inAppReview.requestReview();
-                    }
-                  } catch (e) {
-                    print('에러');
-                  }
-                },
-              ),
-              SettingsTile(
-                title: '이야기 후원하기',
-                subtitle: '개발중입니다.',
-                leading: Icon(Icons.support),
-                titleTextStyle: settingTitleStyle,
-                onTap: () {
-                  Get.defaultDialog(
-                    title: '후원하기',
-                    middleText: '눌러주셔서 고마워요.\n나중에 준비되면 다시 한번 눌러주세요.',
-                    confirmTextColor: Colors.black,
-                    textConfirm: '닫기',
-                    onConfirm: () {
-                      Get.back();
+      body: FutureBuilder(
+        future: SharedPreferences.getInstance(),
+        builder:
+            (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Container();
+          }
+          SharedPreferences _prefs = snapshot.data;
+          return SettingsList(
+            backgroundColor: Colors.grey.shade50,
+            shrinkWrap: true,
+            sections: [
+              SettingsSection(
+                titleTextStyle: settingSectionTitleStyle,
+                title: '일반 설정',
+                tiles: [
+                  SettingsTile(
+                    title: '언어',
+                    subtitle: locale[snapshot.data.getString('locale')],
+                    leading: Icon(Icons.language),
+                    titleTextStyle: settingTitleStyle,
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        child: SimpleDialog(
+                          title: Text('언어를 선택하세요'),
+                          children:
+                              locale.entries.toList().map((MapEntry mapEntry) {
+                            return SimpleDialogOption(
+                              child: Text(mapEntry.value),
+                              onPressed: () async {
+                                await _prefs.setString('locale', mapEntry.key);
+                                Get.back();
+                              },
+                            );
+                          }).toList(),
+                        ),
+                      );
                     },
-                  );
-                },
+                  ),
+                  SettingsTile(
+                    title: '글꼴',
+                    subtitle: font[snapshot.data.getString('font')],
+                    leading: Icon(Icons.cloud_queue),
+                    titleTextStyle: settingTitleStyle,
+                  ),
+                ],
               ),
-              SettingsTile(
-                title: '이야기 앱 알리기',
-                leading: Icon(Icons.share),
-                titleTextStyle: settingTitleStyle,
-                onTap: () {
-                  Share.share(
-                      '이야기앱을 공유합니다. 오늘부터 조금씩 적어보는건 어떨까요?\nbit.ly/3m7WMc7',
-                      subject: 'SHARE_APP');
-                },
+              SettingsSection(
+                title: '연락',
+                titleTextStyle: settingSectionTitleStyle,
+                tiles: [
+                  SettingsTile(
+                    title: '플레이스토어에 리뷰 남기기',
+                    subtitle: '칭찬과 격려를 남겨주세요.',
+                    leading: Icon(Icons.star_rate),
+                    titleTextStyle: settingTitleStyle,
+                    onTap: () async {
+                      try {
+                        final InAppReview inAppReview = InAppReview.instance;
+                        if (await inAppReview.isAvailable()) {
+                          inAppReview.requestReview();
+                        }
+                      } catch (e) {
+                        print('에러');
+                      }
+                    },
+                  ),
+                  SettingsTile(
+                    title: '이야기 후원하기',
+                    subtitle: '개발중입니다.',
+                    leading: Icon(Icons.support),
+                    titleTextStyle: settingTitleStyle,
+                    onTap: () {
+                      Get.defaultDialog(
+                        title: '후원하기',
+                        middleText: '눌러주셔서 고마워요.\n나중에 준비되면 다시 한번 눌러주세요.',
+                        confirmTextColor: Colors.black,
+                        textConfirm: '닫기',
+                        onConfirm: () {
+                          Get.back();
+                        },
+                      );
+                    },
+                  ),
+                  SettingsTile(
+                    title: '이야기 앱 알리기',
+                    leading: Icon(Icons.share),
+                    titleTextStyle: settingTitleStyle,
+                    onTap: () {
+                      Share.share(
+                          '이야기앱을 공유합니다. 오늘부터 조금씩 적어보는건 어떨까요?\nbit.ly/3m7WMc7',
+                          subject: 'SHARE_APP');
+                    },
+                  ),
+                ],
+              ),
+              SettingsSection(
+                title: '기타',
+                titleTextStyle: settingSectionTitleStyle,
+                tiles: [
+                  SettingsTile(
+                    title: '오픈소스 라이선스',
+                    leading: Icon(Icons.collections_bookmark),
+                    titleTextStyle: settingTitleStyle,
+                    onTap: () {
+                      Get.to(LicensePage());
+                    },
+                  ),
+                ],
               ),
             ],
-          ),
-          SettingsSection(
-            title: '기타',
-            titleTextStyle: settingSectionTitleStyle,
-            tiles: [
-              SettingsTile(
-                title: '오픈소스 라이선스',
-                leading: Icon(Icons.collections_bookmark),
-                titleTextStyle: settingTitleStyle,
-                onTap: () {
-                  Get.to(LicensePage());
-                },
-              ),
-            ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
